3cb0b36eb7fbddb7837f328b28a2c15e59719b9c
skmeyer <sarahbutwritingeditioN@gmail.com>
2025-02-17 19:36:30 -0500

    Init commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)
+    {
+        Random gen = new Random();
+        Scanner in = new Scanner(System.in);
+        int die1 = 0;
+        int die2 = 1;
+        String continueYN = "";
+
+        do
+        {
+            System.out.println("Welcome to Craps! Let's start the game.");
+
+        }while(continueYN.equalsIgnoreCase("Y"));
     }
 }
