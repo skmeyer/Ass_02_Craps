9236a671ac421f30690028384ba5629e13906ddc
skmeyer <sarahbutwritingeditioN@gmail.com>
2025-02-17 19:48:18 -0500

    Commit

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,30 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int die1 = 0;
         int die2 = 1;
+        int rollSum = die1 + die2;
+        int rollNum = 0;
         String continueYN = "";
 
         do
         {
             System.out.println("Welcome to Craps! Let's start the game.");
+            while(die1 != die2)
+            {
+                rollNum ++;
+                die1 = gen.nextInt(6) + 1;
+                die2 = gen.nextInt(6) + 1;
+                rollSum = die1 + die2;
+                if(rollSum == 2 || rollSum == 3 || rollSum == 12)
+                {
+                    System.out.println("You crapped out! You rolled a " + rollSum + ".");
+                }
+                else if(rollSum == 7 || rollSum == 11)
+                {
+                    System.out.println("Congrats! You won with a natural. You rolled a " + rollSum + ".");
+                }
+
+            }
+
 
         }while(continueYN.equalsIgnoreCase("Y"));
     }
